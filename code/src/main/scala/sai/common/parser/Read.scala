08e1430c154fd6a71240064c7a54eb79acb01b2c
Guannan Wei <guannanwei@outlook.com>
2018-09-07 14:28:06 -0400

    refactor the parser for core lang

---
+++ code/src/main/scala/sai/common/parser/Read.scala
@@ -0,0 +1,13 @@
+package sai.common.parser
+
+trait Read[A] {
+  def read(s: String): Option[A]
+}
+
+object Read {
+  def apply[A](implicit e: Read[A]): Read[A] = e
+
+  implicit class ReadOps(s: String) {
+    def read[A: Read]: Option[A] = Read[A].read(s)
+  }
+}
--- code/src/main/scala/sai/direct/core/parser/CoreSchemeParser.scala
+++ code/src/main/scala/sai/direct/core/parser/CoreSchemeParser.scala
@@ -2,37 +2,38 @@ package sai.direct.core.parser
 
 import scala.util.parsing.combinator._
 import sai.common.parser._
+import sai.common.parser.Read._
 
 trait CoreSchemeParserTrait extends SchemeTokenParser {
-  def variable: Parser[Var] = IDENT ^^ { Var(_) }
+  implicit def variable: Parser[Var] = IDENT ^^ { Var(_) }
 
-  def app: Parser[App] = LPAREN ~> expr ~ expr <~ RPAREN ^^ {
+  implicit def app: Parser[App] = LPAREN ~> expr ~ expr <~ RPAREN ^^ {
     case e1 ~ e2 => App(e1, e2)
   }
 
-  def lam: Parser[Lam] = LPAREN ~> LAMBDA ~> (LPAREN ~> IDENT <~ RPAREN) ~ expr <~ RPAREN ^^ {
+  implicit def lam: Parser[Lam] = LPAREN ~> LAMBDA ~> (LPAREN ~> IDENT <~ RPAREN) ~ expr <~ RPAREN ^^ {
     case arg ~ body => Lam(arg, body)
   }
 
-  def bind: Parser[Bind] = LPAREN ~> IDENT ~ expr <~ RPAREN ^^ {
+  implicit def bind: Parser[Bind] = LPAREN ~> IDENT ~ expr <~ RPAREN ^^ {
     case id ~ e => Bind(id, e)
   }
 
-  def let: Parser[Expr] = LPAREN ~> LET ~> (LPAREN ~> (LPAREN ~> IDENT ~ expr <~ RPAREN) <~ RPAREN) ~ expr <~ RPAREN ^^ {
+  implicit def let: Parser[Expr] = LPAREN ~> LET ~> (LPAREN ~> (LPAREN ~> IDENT ~ expr <~ RPAREN) <~ RPAREN) ~ expr <~ RPAREN ^^ {
     case id ~ e ~ body => Let(id, e, body)
   }
 
-  def letrec: Parser[Lrc] = LPAREN ~> LETREC ~> (LPAREN ~> bind.+ <~ RPAREN) ~ expr <~ RPAREN ^^ {
+  implicit def letrec: Parser[Lrc] = LPAREN ~> LETREC ~> (LPAREN ~> bind.+ <~ RPAREN) ~ expr <~ RPAREN ^^ {
     case binds ~ body => Lrc(binds, body)
   }
 
-  def lit: Parser[Lit] = INT10 ^^ { Lit(_) }
+  implicit def lit: Parser[Lit] = INT10 ^^ { Lit(_) }
 
-  def if0: Parser[If0] = LPAREN ~> IF0 ~> expr ~ expr ~ expr <~ RPAREN ^^ {
+  implicit def if0: Parser[If0] = LPAREN ~> IF0 ~> expr ~ expr ~ expr <~ RPAREN ^^ {
     case cond ~ th ~ el => If0(cond, th, el)
   }
 
-  def aop: Parser[AOp] = LPAREN ~> PRIMOP ~ expr ~ expr <~ RPAREN ^^ {
+  implicit def aop: Parser[AOp] = LPAREN ~> PRIMOP ~ expr ~ expr <~ RPAREN ^^ {
     case op ~ e1 ~ e2 => AOp(Symbol(op), e1, e2)
   }
 
@@ -40,33 +41,45 @@ trait CoreSchemeParserTrait extends SchemeTokenParser {
 }
 
 object CoreSchemeParser extends CoreSchemeParserTrait {
+  val debug = false
+
   def apply(input: String): Option[Expr] = apply(expr, input)
 
-  def apply[T](pattern: Parser[T], input: String): Option[T] = parse(pattern, input) match {
+  def apply[T](implicit pattern: Parser[T], input: String): Option[T] = parse(pattern, input) match {
     case Success(matched, _) => Some(matched)
-    case e => println(e); None
+    case e => if (debug) println(e); None
+  }
+
+  implicit val ReadStringExpr: Read[Expr] = new Read[Expr] {
+    def read(s: String): Option[Expr] = CoreSchemeParser(s)
+  }
+
+  implicit def ReadString[T <: Expr](implicit pattern: Parser[T]): Read[T] = new Read[T] {
+    def read(s: String): Option[T] = CoreSchemeParser(pattern, s)
   }
 }
 
 object TestCoreSchemeParser {
+  import CoreSchemeParser._
+
   def main(args: Array[String]) = {
-    assert(CoreSchemeParser(CoreSchemeParser.lit, "2") ==
-             Some(Lit(2)))
-    assert(CoreSchemeParser(CoreSchemeParser.if0, "(if0 a b c)") ==
-             Some(If0(Var("a"), Var("b"), Var("c"))))
-    assert(CoreSchemeParser(CoreSchemeParser.if0, "(if0 1 2 3)") ==
-             Some(If0(Lit(1), Lit(2), Lit(3))))
-    assert(CoreSchemeParser("(+ 1 2)") == Some(AOp('+, Lit(1), Lit(2))))
+    assert("1".read[Var] == None)
+    assert("a".read[Lit] == None)
+    assert("2".read[Lit] == Some(Lit(2)))
+    assert("(if0 a b c)".read[If0] == Some(If0(Var("a"), Var("b"), Var("c"))))
+    assert("(if0 1 2 3)".read[If0] == Some(If0(Lit(1), Lit(2), Lit(3))))
+    assert("(+ 1 2)".read[AOp] == Some(AOp('+, Lit(1), Lit(2))))
+    assert("(+ 1 2)".read[Expr] == Some(AOp('+, Lit(1), Lit(2))))
 
     val fact5 = "(letrec ([fact (lambda (n) (if0 n 1 (* n (fact (- n 1)))))]) (fact 5))"
-    assert(CoreSchemeParser(fact5).get ==
-           Lrc(List(Bind("fact",
-                         Lam("n",If0(Var("n"),
-                                   Lit(1),
-                                   AOp('*,
-                                       Var("n"),
-                                       App(Var("fact"),
-                                           AOp('-,Var("n"),Lit(1)))))))),
-               App(Var("fact"),Lit(5))))
+    assert(fact5.read[Expr].get ==
+             Lrc(List(Bind("fact",
+                           Lam("n",If0(Var("n"),
+                                       Lit(1),
+                                       AOp('*,
+                                           Var("n"),
+                                           App(Var("fact"),
+                                               AOp('-,Var("n"),Lit(1)))))))),
+                 App(Var("fact"),Lit(5))))
   }
 }
