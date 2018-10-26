1d6c5d8f40d9c3cf0325f108bfa33fbbc6204882
Guannan Wei <guannanwei@outlook.com>
2018-10-26 16:57:23 -0400

    LMS supports of set and map

--- code/src/main/scala/sai/common/ImmSetOps.scala
+++ code/src/main/scala/sai/common/ImmSetOps.scala
@@ -11,30 +11,39 @@ trait SetOps extends Base with Variables {
   implicit def setTyp[T:Typ]: Typ[Set[T]]
 
   object Set {
-    def apply[A:Typ](xs: Rep[A]*)(implicit pos: SourceContext) = Set_new[A](xs)
+    def apply[A:Typ](xs: Rep[A]*)(implicit pos: SourceContext) = set_new[A](xs)
   }
 
   implicit def repSetToSetOps[A:Typ](v: Rep[Set[A]]) = new SetOpsCls(v)
 
   class SetOpsCls[A:Typ](s: Rep[Set[A]]) {
-    def ++(s1: Rep[Set[A]])(implicit pos: SourceContext) = Set_concat(s, s1)
-    def isEmpty()(implicit pos: SourceContext) = Set_isEmpty(s)
-    def head()(implicit pos: SourceContext) = Set_head(s)
-    def tail()(implicit pos: SourceContext) = Set_tail(s)
-    def foldLeft[B:Typ](z: Rep[B])(f: Rep[((B, A)) => B])(implicit pos: SourceContext) = Set_foldLeft(s, z, f)
+    def ++(s1: Rep[Set[A]])(implicit pos: SourceContext) = set_concat(s, s1)
+    def isEmpty()(implicit pos: SourceContext) = set_isEmpty(s)
+    def head()(implicit pos: SourceContext) = set_head(s)
+    def tail()(implicit pos: SourceContext) = set_tail(s)
+    def intersect(s1: Rep[Set[A]])(implicit pos: SourceContext) = set_intersect(s, s1)
+    def union(s1: Rep[Set[A]])(implicit pos: SourceContext) = set_union(s, s1)
+    def subsetOf(s1: Rep[Set[A]])(implicit pos: SourceContext) = set_subsetof(s, s1)
+    def foldLeft[B:Typ](z: Rep[B])(f: Rep[((B, A)) => B])(implicit pos: SourceContext) = set_foldLeft(s, z, f)
   }
 
-  def Set_new[A:Typ](xs: Seq[Rep[A]])(implicit pos: SourceContext): Rep[Set[A]]
+  def set_new[A:Typ](xs: Seq[Rep[A]])(implicit pos: SourceContext): Rep[Set[A]]
 
-  def Set_concat[A:Typ](s1: Rep[Set[A]], s2: Rep[Set[A]])(implicit pos: SourceContext): Rep[Set[A]]
+  def set_concat[A:Typ](s1: Rep[Set[A]], s2: Rep[Set[A]])(implicit pos: SourceContext): Rep[Set[A]]
 
-  def Set_isEmpty[A:Typ](s: Rep[Set[A]])(implicit pos: SourceContext): Rep[Boolean]
+  def set_isEmpty[A:Typ](s: Rep[Set[A]])(implicit pos: SourceContext): Rep[Boolean]
 
-  def Set_head[A:Typ](s: Rep[Set[A]])(implicit pos: SourceContext): Rep[A]
+  def set_head[A:Typ](s: Rep[Set[A]])(implicit pos: SourceContext): Rep[A]
 
-  def Set_tail[A:Typ](s: Rep[Set[A]])(implicit pos: SourceContext): Rep[Set[A]]
+  def set_tail[A:Typ](s: Rep[Set[A]])(implicit pos: SourceContext): Rep[Set[A]]
 
-  def Set_foldLeft[A:Typ,B:Typ](s: Rep[Set[A]], z: Rep[B], f: Rep[((B, A)) => B])(implicit pos: SourceContext): Rep[B]
+  def set_intersect[A:Typ](s1: Rep[Set[A]], s2: Rep[Set[A]])(implicit pos: SourceContext): Rep[Set[A]]
+
+  def set_union[A:Typ](s1: Rep[Set[A]], s2: Rep[Set[A]])(implicit pos: SourceContext): Rep[Set[A]]
+
+  def set_subsetof[A:Typ](s1: Rep[Set[A]], s2: Rep[Set[A]])(implicit pos: SourceContext): Rep[Boolean]
+
+  def set_foldLeft[A:Typ,B:Typ](s: Rep[Set[A]], z: Rep[B], f: Rep[((B, A)) => B])(implicit pos: SourceContext): Rep[B]
 }
 
 trait SetOpsExp extends SetOps with EffectExp with VariablesExp with BooleanOpsExp with TupledFunctionsExp {
@@ -53,28 +62,40 @@ trait SetOpsExp extends SetOps with EffectExp with VariablesExp with BooleanOpsE
 
   case class SetTail[A:Typ](s: Exp[Set[A]]) extends Def[Set[A]]
 
+  case class SetIntersect[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]]) extends Def[Set[A]]
+
+  case class SetUnion[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]]) extends Def[Set[A]]
+
+  case class SetSubsetOf[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]]) extends Def[Boolean]
+
   case class SetFoldLeft[A:Typ,B:Typ](s: Exp[Set[A]], z: Exp[B], f: Exp[((B, A)) => B]) extends Def[B]
 
-  def Set_new[A:Typ](xs: Seq[Exp[A]])(implicit pos: SourceContext) = SetNew(xs, typ[A])
+  def set_new[A:Typ](xs: Seq[Exp[A]])(implicit pos: SourceContext) = SetNew(xs, typ[A])
 
-  def Set_concat[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]])(implicit pos: SourceContext) = SetConcat(s1, s2)
+  def set_concat[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]])(implicit pos: SourceContext) = SetConcat(s1, s2)
 
-  def Set_isEmpty[A:Typ](s: Exp[Set[A]])(implicit pos: SourceContext) = SetIsEmpty(s)
+  def set_isEmpty[A:Typ](s: Exp[Set[A]])(implicit pos: SourceContext) = SetIsEmpty(s)
 
-  def Set_head[A:Typ](s: Exp[Set[A]])(implicit pos: SourceContext) = SetHead(s)
+  def set_head[A:Typ](s: Exp[Set[A]])(implicit pos: SourceContext) = SetHead(s)
 
-  def Set_tail[A:Typ](s: Exp[Set[A]])(implicit pos: SourceContext) = SetTail(s)
+  def set_tail[A:Typ](s: Exp[Set[A]])(implicit pos: SourceContext) = SetTail(s)
+  
+  def set_intersect[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]])(implicit pos: SourceContext) = SetIntersect(s1, s2)
 
-  def Set_foldLeft[A:Typ,B:Typ](s: Exp[Set[A]], z: Exp[B], f: Exp[((B, A)) => B])(implicit pos: SourceContext) =
+  def set_union[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]])(implicit pos: SourceContext) = SetUnion(s1, s2)
+
+  def set_subsetof[A:Typ](s1: Exp[Set[A]], s2: Exp[Set[A]])(implicit pos: SourceContext) = SetSubsetOf(s1, s2)
+
+  def set_foldLeft[A:Typ,B:Typ](s: Exp[Set[A]], z: Exp[B], f: Exp[((B, A)) => B])(implicit pos: SourceContext) =
     SetFoldLeft(s, z, f)
 }
 
-trait BaseGenListOps extends GenericNestedCodegen {
+trait BaseGenSetOps extends GenericNestedCodegen {
   val IR: SetOpsExp
   import IR._
 }
 
-trait ScalaGenSetOps extends BaseGenListOps with ScalaGenEffect {
+trait ScalaGenSetOps extends BaseGenSetOps with ScalaGenEffect {
   val IR: SetOpsExp
   import IR._
 
@@ -84,6 +105,9 @@ trait ScalaGenSetOps extends BaseGenListOps with ScalaGenEffect {
     case SetIsEmpty(s) => emitValDef(sym, src"$s.isEmpty")
     case SetHead(s) => emitValDef(sym, src"$s.head")
     case SetTail(s) => emitValDef(sym, src"$s.tail")
+    case SetIntersect(s1, s2) => emitValDef(sym, src"$s1.intersect($s2)")
+    case SetUnion(s1, s2) => emitValDef(sym, src"$s1.union($s2)")
+    case SetSubsetOf(s1, s2) => emitValDef(sym, src"$s1.subsetOf($s2)")
     case SetFoldLeft(s, z, f) => emitValDef(sym, src"$s.foldLeft($z)($f)")
     case _ => super.emitNode(sym, rhs)
   }
--- code/src/main/scala/sai/common/MapOps.scala
+++ code/src/main/scala/sai/common/MapOps.scala
@@ -1,11 +1,11 @@
 package sai.common
 
-import scala.lms.common._
 import scala.lms.tutorial._
 import scala.reflect.SourceContext
 import scala.lms.internal.GenericNestedCodegen
+import scala.lms.common.{SetOpsExp ⇒ _, ScalaGenSetOps ⇒ _, _}
 
-trait MapOps extends Base with Variables {
+trait MapOps extends Base with Variables with SetOps {
   implicit def mapTyp[K:Typ, V:Typ]: Typ[Map[K,V]]
 
   object Map {
@@ -17,16 +17,26 @@ trait MapOps extends Base with Variables {
   class MapOpsCls[K:Typ,V:Typ](m: Rep[Map[K,V]]) {
     def apply(k: Rep[K])(implicit pos: SourceContext) = map_apply(m, k)
 
+    def contains(k: Rep[K])(implicit pos: SourceContext) = map_contains(m, k)
+
     def getOrElse(k: Rep[K], default: Rep[V])(implicit pos: SourceContext) = map_getorelse(m, k, default)
 
     def size()(implicit pos: SourceContext) = map_size(m)
 
+    def +(kv: (Rep[K],Rep[V]))(implicit pos: SourceContext) = map_add(m, kv)
+
     def ++(m1: Rep[Map[K,V]])(implicit pos: SourceContext) = map_concat(m, m1)
 
     def ==(m1: Rep[Map[K,V]])(implicit pos: SourceContext) = map_equal(m, m1)
+
+    def keySet()(implicit pos: SourceContext) = map_keyset(m)
+
+    def foldLeft[B:Typ](z: Rep[B])(f: Rep[(B, (K,V)) => B])(implicit pos: SourceContext) = map_foldleft(m, z, f)
   }
 
   def map_apply[K:Typ,V:Typ](m: Rep[Map[K,V]], k: Rep[K])(implicit pos: SourceContext): Rep[V]
+
+  def map_contains[K:Typ,V:Typ](m: Rep[Map[K,V]], k: Rep[K])(implicit pos: SourceContext): Rep[Boolean]
 
   def map_new[K:Typ,V:Typ](kv: Seq[(Rep[K],Rep[V])])(implicit pos: SourceContext): Rep[Map[K, V]]
 
@@ -34,12 +44,18 @@ trait MapOps extends Base with Variables {
 
   def map_getorelse[K:Typ,V:Typ](m: Rep[Map[K,V]], k: Rep[K], default: Rep[V])(implicit pos: SourceContext): Rep[V]
 
+  def map_add[K:Typ,V:Typ](m: Rep[Map[K,V]], kv: (Rep[K],Rep[V]))(implicit pos: SourceContext): Rep[Map[K,V]]
+
   def map_concat[K:Typ,V:Typ](m1: Rep[Map[K,V]], m2: Rep[Map[K,V]])(implicit pos: SourceContext): Rep[Map[K,V]]
 
   def map_equal[K:Typ,V:Typ](m1: Rep[Map[K,V]], m2: Rep[Map[K,V]])(implicit pos: SourceContext): Rep[Boolean]
+
+  def map_keyset[K:Typ,V:Typ](m: Rep[Map[K,V]])(implicit pos: SourceContext): Rep[Set[K]]
+
+  def map_foldleft[K:Typ,V:Typ,B:Typ](m: Rep[Map[K,V]], z: Rep[B], f: Rep[(B, (K,V)) => B])(implicit pos: SourceContext): Rep[B]
 }
 
-trait MapOpsExp extends MapOps with EffectExp with VariablesExp with BooleanOpsExp {
+trait MapOpsExp extends MapOps with EffectExp with VariablesExp with BooleanOpsExp with SetOpsExp {
   implicit def mapTyp[K:Typ,V:Typ]: Typ[Map[K,V]] = {
     implicit val ManifestTyp(k) = typ[K]
     implicit val ManifestTyp(v) = typ[V]
@@ -48,17 +64,27 @@ trait MapOpsExp extends MapOps with EffectExp with VariablesExp with BooleanOpsE
 
   case class MapApply[K:Typ,V:Typ](m: Exp[Map[K,V]], k: Exp[K]) extends Def[V]
 
+  case class MapContains[K:Typ,V:Typ](m: Exp[Map[K,V]], k: Exp[K]) extends Def[Boolean]
+
   case class MapNew[K:Typ,V:Typ](kv: Seq[(Exp[K],Exp[V])], mK: Typ[K], mV: Typ[V]) extends Def[Map[K,V]]
 
   case class MapSize[K:Typ,V:Typ](m: Exp[Map[K,V]]) extends Def[Int]
 
   case class MapGetOrElse[K:Typ,V:Typ](m: Exp[Map[K,V]], k: Exp[K], v: Exp[V]) extends Def[V]
 
+  case class MapAdd[K:Typ,V:Typ](m: Exp[Map[K,V]], kv: (Exp[K],Exp[V])) extends Def[Map[K,V]]
+
   case class MapConcat[K:Typ,V:Typ](m1: Exp[Map[K,V]], m2: Exp[Map[K,V]]) extends Def[Map[K,V]]
 
   case class MapEqual[K:Typ,V:Typ](m1: Exp[Map[K,V]], m2: Exp[Map[K,V]]) extends Def[Boolean]
 
+  case class MapKeySet[K:Typ,V:Typ](m: Exp[Map[K,V]]) extends Def[Set[K]]
+
+  case class MapFoldLeft[K:Typ,V:Typ,B:Typ](m: Exp[Map[K,V]], z: Exp[B], f: Exp[(B, (K,V)) => B])(implicit pos: SourceContext) extends Def[B]
+
   def map_apply[K:Typ,V:Typ](m: Exp[Map[K,V]], k:Exp[K])(implicit pos: SourceContext) = MapApply(m, k)
+
+  def map_contains[K:Typ,V:Typ](m: Exp[Map[K,V]], k:Exp[K])(implicit pos: SourceContext) = MapContains(m, k)
 
   def map_new[K:Typ,V:Typ](kv: Seq[(Exp[K],Exp[V])])(implicit pos: SourceContext) = MapNew(kv, typ[K], typ[V])
 
@@ -67,9 +93,15 @@ trait MapOpsExp extends MapOps with EffectExp with VariablesExp with BooleanOpsE
   def map_getorelse[K:Typ,V:Typ](m: Exp[Map[K,V]], k: Exp[K], default: Exp[V])(implicit pos: SourceContext) =
     MapGetOrElse(m, k, default)
 
+  def map_add[K:Typ,V:Typ](m: Exp[Map[K,V]], kv: (Rep[K],Rep[V]))(implicit pos: SourceContext) = MapAdd(m, kv)
+
   def map_concat[K:Typ,V:Typ](m1: Exp[Map[K,V]], m2: Exp[Map[K,V]])(implicit pos: SourceContext) = MapConcat(m1, m2)
 
   def map_equal[K:Typ,V:Typ](m1: Exp[Map[K,V]], m2: Exp[Map[K,V]])(implicit pos: SourceContext) = MapEqual(m1, m2)
+
+  def map_keyset[K:Typ,V:Typ](m: Exp[Map[K,V]])(implicit pos: SourceContext) = MapKeySet(m)
+
+  def map_foldleft[K:Typ,V:Typ,B:Typ](m: Exp[Map[K,V]], z: Exp[B], f: Exp[(B, (K,V)) => B])(implicit pos: SourceContext) = MapFoldLeft(m, z, f)
 }
 
 trait BaseGenMapOps extends GenericNestedCodegen {
@@ -77,17 +109,21 @@ trait BaseGenMapOps extends GenericNestedCodegen {
   import IR._
 }
 
-trait ScalaGenMapOps extends BaseGenMapOps with ScalaGenEffect {
+trait ScalaGenMapOps extends BaseGenMapOps with ScalaGenEffect with ScalaGenSetOps {
   val IR: MapOpsExp
   import IR._
 
   override def emitNode(sym: Sym[Any], rhs: Def[Any]) = rhs match {
     case MapNew(kv, mk, mv) => emitValDef(sym, src"Map[$mk, $mv](${(kv.map(kv => "("+quote(kv._1)+","+quote(kv._2)+")")).mkString(",")})")
+    case MapContains(m, k) => emitValDef(sym, src"$m.contains($k)")
     case MapApply(m, k) => emitValDef(sym, src"$m($k)")
     case MapSize(m) => emitValDef(sym, src"$m.size")
     case MapGetOrElse(m, k, d) => emitValDef(sym, src"$m.getOrElse($k, $d)")
+    case MapAdd(m, kv) => emitValDef(sym, src"$m + (${kv._1} -> ${kv._2})")
     case MapConcat(m1, m2) => emitValDef(sym, src"$m1 ++ $m2")
     case MapEqual(m1, m2) => emitValDef(sym, src"$m1 == $m2")
+    case MapKeySet(m) => emitValDef(sym, src"$m.keySet")
+    case MapFoldLeft(m, z, f) => emitValDef(sym, src"$m.foldLeft($z)($f)")
     case _ => super.emitNode(sym, rhs)
   }
 }
