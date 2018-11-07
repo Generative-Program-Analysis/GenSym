b4412c5a632ed9dcc0e89a2b66aa4210c7abe98a
Yuxuan Chen <ychen@yuxuanlabs.com>
2018-11-06 21:33:05 -0500

    Add interpreter pipeline

---
+++ code/src/main/scala/sai/direct/large/Interpreter.scala
@@ -0,0 +1,19 @@
+package sai.direct.large
+
+import sai.direct.large.parser._
+import sai.direct.large.concrete._
+
+object LargeSchemeInterpreter {
+  def apply(s: String) = {
+    val ast = LargeSchemeParser(s) match {
+      case Some(expr) => LargeSchemeASTDesugar(expr)
+    }
+    BigStepCES.eval(ast)
+  }
+
+  def main(args: Array[String]) = {
+    if (!args.isEmpty) {
+      println(apply(args(0)))
+    }
+  }
+}
