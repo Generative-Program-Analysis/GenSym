7b1363f7d79ce967118596f17e50d92344377055
Guannan Wei <kiss.kraks@gmail.com>
2019-02-12 03:13:44 -0500

    code & revision

--- code/src/main/scala/sai/direct/core/ai/Monadic.scala
+++ code/src/main/scala/sai/direct/core/ai/Monadic.scala
@@ -33,14 +33,72 @@ object Lang {
 object Monadics {
   import Lang._
 
+  /*
+   trait M[+A] {
+   def unit[A](a: A): M[A]
+   def flatMap[B](f: A => M[B]): M[B]
+   def map[B](f: A => B): M[B]
+   }
+
+   object M {
+   def apply[A](implicit v: M[A]): M[A] = v
+   }
+  */
+
   trait Monad[M[_]] {
     def unit[A](a: A): M[A]
-    def bind[A, B](m: M[A])(f: A => M[B]): M[B]
+    def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+
+    def map[A,B](ma: M[A])(f: A => B): M[B] = flatMap(ma)(a => unit(f(a)))
+    def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
+      flatMap(ma)(a => map(mb)(b => f(a, b)))
   }
 
-  // TODO: abstract values
+  object Monad {
+    def apply[M[_]](implicit v: Monad[M]): Monad[M] = v
+  }
 
-  trait Semantics[M[_]] { self: Monad[M] =>
+  implicit class MonadOps[A, M[_]: Monad](ma: M[A]) {
+    def map[B](f: A => B): M[B] = Monad[M].map(ma)(f)
+    def flatMap[B](f: A => M[B]): M[B] = Monad[M].flatMap(ma)(f)
+  }
+
+  type Id[T] = T
+  def IdMonad[T]: Monad[Id] = new Monad[Id] {
+    def unit[A](a: A): A = a
+    def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = f(ma)
+  }
+
+  trait Semantics {
+    type Env
+    type Value
+    type M[Value] <: MonadOps[Value, M]
+
+    def num(i: Int): M[Value]
+    def close(lam: Lam, env: Env): M[Value]
+    def add(v1: Value, v2: Value): M[Value]
+    def apply(v1: Value, v2: Value): M[Value]
+    def lookup(x: Name, env: Env): M[Value]
+    def extend(env: Env, x: Name, v: Value): Env
+
+    def interp(e: Term, env: Env): M[Value] = e match {
+      case Con(i) => num(i)
+      case Var(x) => lookup(x, env)
+      case Lam(x, e) => close(Lam(x, e), env)
+      case Add(e1, e2) => for {
+        v1 <- interp(e1, env)
+        v2 <- interp(e2, env)
+        rt <- add(v1, v2)
+      } yield rt
+      case App(e1, e2) => for {
+        v1 <- interp(e1, env)
+        v2 <- interp(e2, env)
+        rt <- apply(v1, v2)
+      } yield rt
+    }
+  }
+
+  trait Concrete extends Semantics {
     trait Value
     case object Wrong extends Value
     case class Num(i: Int) extends Value
@@ -48,41 +106,42 @@ object Monadics {
 
     type Env = Map[Name, Value]
 
-    def lookup(x: Name, env: Env): M[Value] = env.get(x) match {
-      case Some(v) => unit(v)
-      case None => unit(Wrong)
+    def num(i: Int): M[Value] = Monad[M].unit(Num(i))
+
+    /*
+    def close(lam: Lam, env: Env): M[Value] = lam match {
+      case Lam(x, e) => M[Value].unit(Fun(v => interp(e, extend(env, x, v))))
     }
 
+    def lookup(x: Name, env: Env): M[Value] = env.get(x) match {
+      case Some(v) => M[Value].unit(v)
+      case None => M[Value].unit(Wrong)
+    }
+
+    def extend(env: Env, x: Name, v: Value) = env + (x -> v)
+
     def add(v1: Value, v2: Value): M[Value] = (v1, v2) match {
-      case (Num(x), Num(y)) => unit(Num(x + y))
-      case _ => unit(Wrong)
+      case (Num(x), Num(y)) => M[Value].unit(Num(x + y))
+      case _ => M[Value].unit(Wrong)
     }
 
     def apply(v1: Value, v2: Value): M[Value] = (v1, v2) match {
       case (Fun(k), a) => k(a)
-      case _ => unit(Wrong)
+      case _ => M[Value].unit(Wrong)
     }
-
-    def interp(e: Term, env: Env): M[Value] = e match {
-      case Var(x) => lookup(x, env)
-      case Con(i) => unit(Num(i))
-      case Add(e1, e2) =>
-        bind(interp(e1, env))(v1 => bind(interp(e2, env))(v2 => add(v1, v2)))
-      case Lam(x, e) => unit(Fun(v => interp(e, env + (x -> v))))
-      case App(e1, e2) =>
-        bind(interp(e1, env))(v1 => bind(interp(e2, env))(v2 => apply(v1, v2)))
-    }
+     */
   }
 
   /****************************************************/
 
+  /*
   type Id[T] = T
-  trait IdMonad extends Monad[Id] {
+  trait IdMonad[A] extends M[A] {
     def unit[A](a: A) = a
     def bind[A, B](m: Id[A])(f: A => Id[B]) = f(m)
   }
 
-  case class StandardInterp() extends Semantics[Id] with IdMonad
+  case class StandardInterp() extends Concrete with IdMonad
 
   /****************************************************/
 
@@ -98,7 +157,7 @@ object Monadics {
     }
   }
 
-  case class ErrMsgInterp() extends Semantics[ErrMsg] with ErrMonad
+  case class ErrMsgInterp() extends Concrete[ErrMsg] with ErrMonad
 
   /****************************************************/
 
@@ -112,7 +171,7 @@ object Monadics {
     }
   }
 
-  case class StateInterp() extends Semantics[IntState] with StateMonad
+  case class StateInterp() extends Concrete[IntState] with StateMonad
 
   /****************************************************/
 
@@ -122,11 +181,12 @@ object Monadics {
       for (a <- m; b <- f(a)) yield b
   }
 
-  case class NDInterp() extends Semantics[List] with NDMonad {
+  case class NDInterp() extends Concrete[List] with NDMonad {
     override def interp(e: Term, env: Env) = e match {
       case Fail() => List()
       case Amb(e1, e2) => interp(e1, env) ++ interp(e2, env)
       case _ => super.interp(e, env)
     }
   }
+   */
 }
