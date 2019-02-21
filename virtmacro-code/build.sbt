0367c7ccdc813e2e9447dfa74e35e1fa5f853d46
Guannan Wei <guannanwei@outlook.com>
2019-02-20 23:33:15 -0500

    moandic interp framework, new code with macro-virt lms

---
+++ virtmacro-code/build.sbt
@@ -0,0 +1,33 @@
+import Dependencies._
+
+ThisBuild / scalaVersion     := "2.12.7"
+ThisBuild / version          := "0.1-ICFP"
+ThisBuild / organization     := "sai"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "SAI",
+    autoCompilerPlugins := true,
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+
+    addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.9"),
+    addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full),
+    addCompilerPlugin("org.scala-lang.plugins" % "scala-continuations-plugin_2.12.0" % "1.0.3"),
+
+    libraryDependencies += scalaTest % Test,
+    libraryDependencies += "org.scala-lang.lms" %% "lms-core-macrovirt" % "0.9.0-SNAPSHOT",
+    libraryDependencies += "org.scala-lang" % "scala-compiler" % scalaVersion.value % "compile",
+    libraryDependencies += "org.scala-lang" % "scala-library" % scalaVersion.value % "compile",
+    libraryDependencies += "org.scala-lang" % "scala-reflect" % scalaVersion.value % "compile",
+    libraryDependencies += "org.scala-lang.plugins" % "scala-continuations-library_2.12" % "1.0.3",
+    libraryDependencies += "org.typelevel" %% "cats-core" % "1.6.0",
+    libraryDependencies += "org.typelevel" %% "cats-free" % "1.6.0",
+    libraryDependencies += "com.github.mpilquist" %% "simulacrum" % "0.15.0",
+    libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.6",
+
+    scalacOptions += "-P:continuations:enable",
+    scalacOptions += "-Ypartial-unification",
+
+    concurrentRestrictions in Global += Tags.limit(Tags.Test, 1)
+  )
+
---
+++ virtmacro-code/src/main/scala/sai/Main.scala
@@ -0,0 +1,258 @@
+package sai
+
+import cats._
+import cats.{Id, ~>}
+import cats.implicits._
+import cats.data.State
+import cats.data.IndexedStateT
+import cats.data.Kleisli
+import cats.free.Free
+import cats.free.Free.liftF
+import cats.arrow.FunctionK
+import cats.syntax.applicative._
+import scala.collection.mutable
+
+object DSL {
+  /* An DSL example using free monads */
+  sealed trait KVStoreA[A]
+  case class Put[T](key: String, value: T) extends KVStoreA[Unit]
+  case class Get[T](key: String) extends KVStoreA[Option[T]]
+  case class Del(key: String) extends KVStoreA[Unit]
+
+  type KVStore[A] = Free[KVStoreA, A]
+
+  def put[T](key: String, value: T): KVStore[Unit] =
+    liftF[KVStoreA, Unit](Put[T](key, value))
+  def get[T](key: String): KVStore[Option[T]] =
+    liftF[KVStoreA, Option[T]](Get[T](key))
+  def del(key: String): KVStore[Unit] =
+    liftF(Del(key))
+  def update[T](key: String, f: T => T): KVStore[Unit] =
+    for {
+      v <- get[T](key)
+      _ <- v.map(v => put[T](key, f(v))).getOrElse(Free.pure(()))
+    } yield ()
+
+  def impureCompiler: KVStoreA ~> Id = new (KVStoreA ~> Id) {
+    val kvs = mutable.Map.empty[String, Any]
+    def apply[A](fa: KVStoreA[A]): Id[A] =
+      fa match {
+        case Put(key, value) =>
+          kvs(key) = value
+          ()
+        case Get(key) =>
+          kvs.get(key).map(_.asInstanceOf[A])
+        case Del(key) =>
+          kvs.remove(key)
+          ()
+      }
+  }
+
+  type KVStoreState[A] = State[Map[String, Any], A]
+  def pureCompiler: KVStoreA ~> KVStoreState = new (KVStoreA ~> KVStoreState) {
+    def apply[A](fa: KVStoreA[A]): KVStoreState[A] = fa match {
+      case Put(key, value) => State.modify(_.updated(key, value))
+      case Get(key) => State.inspect(_.get(key).map(_.asInstanceOf[A]))
+      case Del(key) => State.modify(_ - key)
+    }
+  }
+
+  def program: KVStore[Option[Int]] =
+    for {
+      _ <- put("wild-cats", 1)
+      _ <- update[Int]("wild-cats", (_ + 12))
+      _ <- put("tame-cats", 2)
+      n <- get[Int]("wild-cats")
+      _ <- del("tame-cats")
+    } yield n
+
+  val result0: Option[Int] = program.foldMap(impureCompiler)
+  val result1: (Map[String, Any], Option[Int]) = program.foldMap(pureCompiler).run(Map.empty).value
+}
+
+object PCFLang {
+  sealed trait Expr
+  sealed trait AExpr extends Expr
+  sealed trait CExpr extends Expr
+  case class Lit(i: Int) extends AExpr
+  case class Var(x: String) extends AExpr
+  case class Lam(x: String, e: Expr) extends AExpr
+  case class App(e1: Expr, e2: Expr) extends CExpr
+  case class Let(x: String, rhs: Expr, body: Expr) extends CExpr
+  case class Rec(x: String, rhs: Expr, body: Expr) extends CExpr
+  case class If0(e1: Expr, e2: Expr, e3: Expr) extends CExpr
+  case class Aop(op: Symbol, e1: Expr, e2: Expr) extends CExpr
+
+  object Values {
+    sealed trait Value
+    case class IntV(i: Int) extends Value
+    case class CloV[Env](lam: Lam, e: Env) extends Value
+  }
+
+  val p1 = Let("x", Lit(1),
+               Let("y", Lit(2),
+                   Let("f", Lam("z", Aop('+, Var("z"), Var("y"))),
+                       App(Var("f"), Var("x")))))
+}
+
+object Misc {
+  type Reader[A, B] = Kleisli[Id, A, B]
+  object Reader {
+    def apply[A, B](f: A => B): Reader[A, B] = Kleisli[Id, A, B](f)
+  }
+}
+
+object EnvInterpreter {
+  /* An environment interpreter using Reader Monad */
+  import PCFLang._
+  import PCFLang.Values._
+  type Ident = String
+  type Env = Map[Ident, Value]
+
+  type ReaderT[F[_], A, B] = Kleisli[F, A, B]
+  val ReaderT = Kleisli
+  import ReaderT._
+
+  type Ans = ReaderT[Id, Env, Value]
+
+  def ap_clo(fun: Value, arg: Value): Ans = fun match {
+    case CloV(Lam(x, e), ρ: Env) => eval(e).local[Env](_ => ρ + (x → arg))
+  }
+
+  def branch0(test: Value, thn: Expr, els: Expr): Ans =
+    if (test == IntV(0)) eval(thn) else eval(els)
+
+  def prim(op: Symbol, v1: Value, v2: Value): Value = (op, v1, v2) match {
+    case ('+, IntV(x), IntV(y)) => IntV(x + y)
+    case ('-, IntV(x), IntV(y)) => IntV(x - y)
+    case ('*, IntV(x), IntV(y)) => IntV(x * y)
+    case ('/, IntV(x), IntV(y)) => IntV(x / y)
+  }
+
+  def eval(e: Expr): Ans = e match {
+    case Lit(i) => pure(IntV(i))
+    case Var(x) => for {
+      ρ <- ask[Id, Env]
+    } yield ρ(x)
+    case Lam(x, e) => for {
+      ρ <- ask[Id, Env]
+    } yield CloV(Lam(x, e), ρ)
+    case App(e1, e2) => for {
+      v1 <- eval(e1)
+      v2 <- eval(e2)
+      rt <- ap_clo(v1, v2)
+    } yield rt
+    case Let(x, rhs, e) => for {
+      v <- eval(rhs)
+      ρ <- ask[Id, Env]
+      rt <- eval(e).local[Env](_ => ρ + (x → v))
+    } yield rt
+    case If0(e1, e2, e3) => for {
+      cnd <- eval(e1)
+      rt <- branch0(cnd, e2, e3)
+    } yield rt
+    case Aop(op, e1, e2) => for {
+      v1 <- eval(e1)
+      v2 <- eval(e2)
+    } yield prim(op, v1, v2)
+    case Rec(x, rhs, e) => ???
+  }
+}
+
+object EnvStoreInterpreter {
+  /* An environment-and-store interpreter using Reader Monad and State Monad */
+  import PCFLang._
+  import PCFLang.Values._
+  type Ident = String
+  type Addr = Int
+  type Env = Map[Ident, Addr]
+  type Store = Map[Addr, Value]
+
+  type ReaderT[F[_], A, B] = Kleisli[F, A, B]
+  val ReaderT = Kleisli
+  //import ReaderT._
+
+  type State[T] = IndexedStateT[Id, Store, Store, T]
+  val StateT = IndexedStateT
+  //import IndexedStateT._
+  type Ans = ReaderT[State, Env, Value]
+
+  def local_env(xv: (String, Addr)): ReaderT[State, Env, Env] = for {
+    ρ <- ReaderT.ask[State, Env]
+  } yield ρ + xv
+
+  def alloc(x: String): ReaderT[State, Env, Addr] = for {
+    σ <- ReaderT.lift(IndexedStateT.get[Id, Store])
+  } yield σ.size + 1
+
+  def ap_clo(fun: Value, arg: Value): Ans = fun match {
+    case CloV(Lam(x, e), ρ: Env) => for {
+      α <- alloc(x)
+      _ <- global_store(α → arg)
+      ρ <- local_env(x → α)
+      rt <- eval(e).local[Env](_ => ρ)
+    } yield rt
+  }
+
+  def global_store(av: (Addr, Value)): ReaderT[State, Env, Unit] = for {
+    _ <- ReaderT.lift(IndexedStateT.modify[Id, Store, Store](σ => σ + av))
+  } yield ()
+
+  def branch0(test: Value, thn: Expr, els: Expr): Ans =
+    if (test == IntV(0)) eval(thn) else eval(els)
+
+  def prim(op: Symbol, v1: Value, v2: Value): Value = (op, v1, v2) match {
+    case ('+, IntV(x), IntV(y)) => IntV(x + y)
+    case ('-, IntV(x), IntV(y)) => IntV(x - y)
+    case ('*, IntV(x), IntV(y)) => IntV(x * y)
+    case ('/, IntV(x), IntV(y)) => IntV(x / y)
+  }
+
+  def eval(e: Expr): Ans = e match {
+    case Lit(i) => ReaderT.pure(IntV(i))
+    case Var(x) => for {
+      ρ <- ReaderT.ask[State, Env]
+      σ <- ReaderT.lift(IndexedStateT.get[Id, Store])
+    } yield σ(ρ(x))
+    case Lam(x, e) => for {
+      ρ <- ReaderT.ask[State, Env]
+    } yield CloV(Lam(x, e), ρ)
+    case App(e1, e2) => for {
+      v1 <- eval(e1)
+      v2 <- eval(e2)
+      rt <- ap_clo(v1, v2)
+    } yield rt
+    case Let(x, rhs, e) => for {
+      v <- eval(rhs)
+      α <- alloc(x)
+      _ <- global_store(α → v)
+      ρ <- local_env(x → α)
+      rt <- eval(e).local[Env](_ => ρ)
+    } yield rt
+    case If0(e1, e2, e3) => for {
+      cnd <- eval(e1)
+      rt <- branch0(cnd, e2, e3)
+    } yield rt
+    case Aop(op, e1, e2) => for {
+      v1 <- eval(e1)
+      v2 <- eval(e2)
+    } yield prim(op, v1, v2)
+    case Rec(x, rhs, e) => for {
+      α <- alloc(x)
+      ρ <- local_env(x → α)
+      v <- eval(rhs).local[Env](_ => ρ)
+      _ <- global_store(α → v)
+      rt <- eval(e).local[Env](_ => ρ)
+    } yield rt
+  }
+
+}
+object Main {
+  def main(args: Array[String]): Unit = {
+    //println("Hello")
+    //println(DSL.result1)
+    import PCFLang._
+    import EnvInterpreter._
+    //println(eval(p1).run(Map()))
+  }
+}
