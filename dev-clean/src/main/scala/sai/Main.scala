eeb9fab4a5c3cf5ef94f69d8e73707cdc242d3b3
Guannan Wei <guannanwei@outlook.com>
2019-11-28 21:35:17 -0500

    WIP to lms-clean

--- dev-clean/src/main/scala/sai/Main.scala
+++ dev-clean/src/main/scala/sai/Main.scala
@@ -4,16 +4,38 @@ import lms.core.stub._
 import lms.macros.SourceContext
 import lms.core.virtualize
 
+import lms.core._
+import lms.util._
+import lms.core.utils.time
+
+import sai.lmsx._
+
+
 object Main {
   def test_power() = {
-    val snippet = new DslDriver[Int, Int] {
+    val snippet = new SAIDriver[List[Int], Int] {
+      @virtualize
       def power(b: Rep[Int], x: Int): Rep[Int] =
         if (x == 0) 1
         else b * power(b, x - 1)
-      def snippet(b: Rep[Int]) = power(b, 10)
+
+      @virtualize
+      def listget(xs: Rep[List[Int]]): Rep[Int] = {
+        /*
+        val a = xs(2) + 1
+        val b = xs(1) + a
+        b
+         */
+        val plusone = xs.map((x: Rep[Int]) => x + 1)
+        val plustwo = xs.map((x: Rep[Int]) => x + 2)
+        plusone(2) + plustwo(1)
+      }
+
+      //def snippet(b: Rep[Int]) = power(b, 10)
+      def snippet(xs: Rep[List[Int]]) = listget(xs)
     }
     println(snippet.code)
-    assert(snippet.eval(2) == 1024)
+    assert(snippet.eval(List(1,2,3)) == 3)
   }
 
   def main(args: Array[String]) {
--- dev-clean/src/main/scala/sai/lms/LMSSupport.scala
+++ dev-clean/src/main/scala/sai/lms/LMSSupport.scala
@@ -1,11 +1,14 @@
 package sai
 package lmsx
 
-import lms.core.stub._
-import lms.macros.SourceContext
-import lms.core.virtualize
 import lms.core._
-import Backend._
+import lms.util._
+import lms.core.stub._
+import lms.core.Backend._
+import lms.core.virtualize
+import lms.core.utils.time
+import lms.macros.SourceContext
+
 
 trait ListOps { b: Base =>
   object List {
@@ -17,13 +20,14 @@ trait ListOps { b: Base =>
   implicit def __liftVarList[A: Manifest](xs: Var[List[A]]): ListOps[A] = new ListOps(readVar(xs))
 
   implicit class ListOps[A: Manifest](xs: Rep[List[A]]) {
-    def apply(i: Rep[Int]): Rep[A] = Wrap[A](Adapter.g.reflect("list_apply", Unwrap(xs), Unwrap(i)))
-    def map[B: Manifest](f: Rep[A] => Rep[B]) = Wrap[List[B]](Adapter.g.reflect("list_map", Unwrap(xs), Unwrap(f)))
+    def apply(i: Rep[Int]): Rep[A] = Wrap[A](Adapter.g.reflect("list-apply", Unwrap(xs), Unwrap(i)))
+    def map[B: Manifest](f: Rep[A] => Rep[B]) = {
+      val block = Adapter.g.reify(x => Unwrap(f(Wrap[A](x))))
+      Wrap[List[B]](Adapter.g.reflect("list-map", Unwrap(xs), block))
+    }
   }
 
   /*
-  class ListOpsCls[A:Manifest](l: Rep[List[A]]) {
-    def map[B:Manifest](f: Rep[A] => Rep[B]) = list_map(l,f)
     def flatMap[B : Manifest](f: Rep[A] => Rep[List[B]]) = list_flatMap(l,f)
     def withFilter(f: Rep[A] => Rep[Boolean]) = list_filter(l, f)
     def filter(f: Rep[A] => Rep[Boolean]) = list_filter(l, f)
@@ -43,20 +47,68 @@ trait ListOps { b: Base =>
     def foldLeftPair[B:Manifest,C:Manifest](z: Rep[(B,C)])(f: ((Rep[B], Rep[C]), Rep[A]) => Rep[(B,C)]) = list_foldLeftPair(l, z, f)
     def containsSlice(that: Rep[List[A]]) = list_containsSlice(l, that)
     def intersect(that: Rep[List[A]]) = list_intersect(l, that)
-  }
   */
 }
 
-class ScalaCodeGen_List extends ScalaCodeGen {
+trait ScalaCodeGen_List extends ExtendedScalaCodeGen {
+  override def shallow(n: Node): Unit = n match {
+    case Node(s, "list-apply", List(xs, i), _) =>
+      shallow(xs); emit("("); shallow(i); emit(")")
+    case Node(s, "list-map", List(xs, b), _) =>
+      //FIXME: emit indented code
+      shallow(xs); emit(".map("); shallow(b); emit(")")
+    case _ => super.shallow(n)
+  }
+
+  //TODO: what should be added here?
   override def traverse(n: Node): Unit = n match {
-    case Node(s, "list_apply", List(xs, i), _) =>
-      emit(s"val $s = ${quote(xs)}(${quote(i)})")
-    case Node(s, "list_map", List(xs, f), _) =>
-      emit(s"val $s = ${quote(xs)}.map(${quote(i)})")
+    /*
+    case Node(s, "list-apply", List(xs, i), _) =>
+      shallow(xs); emit("("); shallow(i); emit(")")
+    case Node(s, "list-map", List(xs, f), _) =>
+      emit(s"val $s = ${quote(xs)}.map(${quote(f)})")
+     */
     case _ => super.traverse(n)
   }
 }
 
-trait SAIDsl extends Dsl
+trait SAIOps extends Base with PrimitiveOps with LiftPrimitives with Equal
+    with OrderingOps with LiftVariables
+    with ListOps {
+  type Typ[T] = Manifest[T]
+  def typ[T: Typ] = manifest[T]
+  def manifestTyp[T: Typ] = manifest[T]
+}
 
-trait SAIOpsExp extends DslExp
+trait SAIDslImpl extends SAIOps { q =>
+  val codegen = new ScalaGenBase with ScalaCodeGen_List {
+    val IR: q.type = q
+    import IR._
+  }
+}
+
+abstract class SAISnippet[A:Manifest, B:Manifest] extends SAIOps {
+  def wrapper(x: Rep[A]): Rep[B] = snippet(x)
+  def snippet(x: Rep[A]): Rep[B]
+}
+
+abstract class SAIDriver[A: Manifest, B: Manifest] extends SAISnippet[A, B] with SAIDslImpl {
+  lazy val (code, statics) = {
+    val source = new java.io.ByteArrayOutputStream()
+    val statics = codegen.emitSource(wrapper, "Snippet",
+      new java.io.PrintStream(source))(manifestTyp[A], manifestTyp[B])
+    (source.toString, statics)
+  }
+
+  lazy val f = {
+    val (c1, s1) = (code, statics)
+    time("scalac") { Global.sc.compile[A, B]("Snippet", c1, s1) }
+  }
+
+  def precompile: Unit = f
+  def precompileSilently: Unit = utils.devnull(f)
+  def eval(x: A): B = {
+    val f1 = f
+    time("eval")(f1(x))
+  }
+}
