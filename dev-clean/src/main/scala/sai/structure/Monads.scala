06ef3611de8c6dce86a6b6ff00b47a95e419b71b
Guannan Wei <kiss.kraks@gmail.com>
2019-12-30 23:37:32 -0500

    rename...

--- dev-clean/src/main/scala/sai/structure/Monads.scala
+++ dev-clean/src/main/scala/sai/structure/Monads.scala
@@ -95,7 +95,7 @@ case class IdM[A](run: A) {
 /* Error Monad */
 
 abstract class Err[+A]
-case object None extends Err[Nothing]
+case object Bad extends Err[Nothing]
 case class Ok[+A](a: A) extends Err[A]
 
 object ErrM {
@@ -108,24 +108,24 @@ object ErrM {
     def mzero[A: Lattice]: ErrM[A] = ErrM(Ok(Lattice[A].bot))
     def mplus[A: Lattice](a: ErrM[A], b: ErrM[A]): ErrM[A] = (a, b) match {
       case (ErrM(Ok(x)), ErrM(Ok(y))) => ErrM(Ok(x ⊔ y))
-      case (ErrM(Ok(x)), ErrM(None)) => ErrM(Ok(x))
-      case (ErrM(None), ErrM(Ok(y))) => ErrM(Ok(y))
-      case (ErrM(None), ErrM(None)) => ErrM(None)
+      case (ErrM(Ok(x)), ErrM(Bad)) => ErrM(Ok(x))
+      case (ErrM(Bad), ErrM(Ok(y))) => ErrM(Ok(y))
+      case (ErrM(Bad), ErrM(Bad)) => ErrM(Bad)
     }
   }
 
-  def err[A]: ErrM[A] = ErrM[A](None)
+  def err[A]: ErrM[A] = ErrM[A](Bad)
 }
 
 case class ErrM[A](run: Err[A]) {
   def apply: Err[A] = run
   def flatMap[B](f: A => ErrM[B])(implicit mB: Manifest[B] = null): ErrM[B] = run match {
     case Ok(a) => f(a)
-    case None => ErrM[B](None)
+    case Bad => ErrM[B](Bad)
   }
   def map[B](f: A => B)(implicit mB: Manifest[B] = null): ErrM[B] = run match {
     case Ok(a) => ErrM(Ok(f(a)))
-    case None => ErrM[B](None)
+    case Bad => ErrM[B](Bad)
   }
 }
 
