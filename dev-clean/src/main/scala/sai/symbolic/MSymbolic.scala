aedbd4a282b19c29521b2e54b0e03bbd42e5b473
Guannan Wei <guannanwei@outlook.com>
2020-01-07 19:51:10 -0500

    update

--- dev-clean/src/main/scala/sai/symbolic/MSymbolic.scala
+++ dev-clean/src/main/scala/sai/symbolic/MSymbolic.scala
@@ -1,9 +1,15 @@
 package sai.msymbolic
 
+import lms.core._
+import lms.core.stub._
+import lms.core.virtualize
+import lms.macros.SourceContext
+
+import sai.lmsx._
+import sai.structure.monad._
+
 import sai.lang.SimpIL
 import sai.lang.SimpIL._
-
-import sai.structure.monad._
 
 object Concrete {
   //import SimpIL.Values._
@@ -161,7 +167,93 @@ object Concrete {
     drive(ProgToMap(p)).run((0, Map(), Map())).run
   }
 
-  def main(args: Array[String]): Unit = {
+}
+
+@virtualize
+trait StagedConcrete extends SAIOps {
+  import sai.lang.SimpIL.{Exp => Expr, _}
+
+  type PC = Int
+  type Addr = Int
+  type Value = Int
+  type Env = Map[String, Value]
+  type Store = Map[Addr, Value]
+  type Prg = Map[PC, Stmt]
+
+  type State = (PC, Env, Store)
+
+  import IdM._
+  import StateT._
+  import EitherT._
+
+  type StateM[T] = StateT[IdM, State, T]
+  type M[T] = EitherT[StateM, Value, T]
+  val M = Monad[M]
+
+  def get_env: M[Env] = EitherT.liftM[StateM, Value, Env](StateTMonad[IdM, State].get.map(s => s._2))
+  def get_store: M[Store] = EitherT.liftM[StateM, Value, Store](StateTMonad[IdM, State].get.map(s => s._3))
+  def update_env(x: Rep[String], v: Rep[Value]): M[Unit] =
+    EitherT.liftM[StateM, Value, Unit](StateTMonad[IdM, State].mod(s => (s._1, s._2 + (x, v), s._3)))
+  def update_store(x: Rep[Addr], v: Rep[Value]): M[Unit] =
+    EitherT.liftM[StateM, Value, Unit](StateTMonad[IdM, State].mod(s => (s._1, s._2, s._3 + (x, v))))
+  def inc_pc: M[Unit] = 
+    EitherT.liftM[StateM, Value, Unit](StateTMonad[IdM, State].mod(s => (s._1 + 1, s._2, s._3)))
+
+  def eval(e: Expr): M[Value] = e match {
+    case Lit(i) => M.pure(i)
+    case Var(x) => for { ρ <- get_env } yield ρ(x)
+    case Load(e) => for {
+      σ <- get_store
+      a <- eval(e)
+    } yield σ(a)
+      /*
+    case BinOp(op, e1, e2) => for {
+      v1 <- eval(e1)
+      v2 <- eval(e2)
+    } yield evalBinOp(op, v1, v2)
+    case UnaryOp(op, e) => for { v <- eval(e) } yield evalUnaryOp(op, v)
+       */
+    //case GetInput(_) => M.pure(scala.io.StdIn.readInt)
+  }
+
+  def exec(s: Stmt): M[Unit] = s match {
+    case Assign(x, e) => for {
+      v <- eval(e)
+      _ <- update_env(x, v)
+      _ <- inc_pc
+    } yield ()
+    case Store(e1, e2) => for {
+      a <- eval(e1)
+      v <- eval(e2)
+      _ <- update_store(a, v)
+      _ <- inc_pc
+    } yield ()
+  }
+
+  def runStmt(s: Stmt): Rep[(Either[Value, Unit], State)] = {
+    val s0: Rep[State] = (0, Map[String, Int](), Map[Int, Int]())
+    exec(s).run(s0).run
+  }
+
+  //def run(prg: List[Stmt])
+}
+
+trait StagedConcreteGen extends SAICodeGenBase
+
+trait StagedConcreteDriver extends SAIDriver[Unit, Unit] with StagedConcrete { q =>
+  override val codegen = new ScalaGenBase with SAICodeGenBase {
+    val IR: q.type = q
+    import IR._
+    override def remap(m: Manifest[_]): String = {
+      super.remap(m)
+    }
+
+    //override val prelude = ""
+  }
+}
+
+object MSymbolicTest {
+  def runUnstagedConcrete = {
     import SimpIL.Examples._
     //println(run(ex1))
 
@@ -170,7 +262,22 @@ object Concrete {
     ))
     //println(run(h))
 
-    println(run(ex3))
-    println(run_smallstep(ex3))
+    println(Concrete.run(ex3))
+    println(Concrete.run_smallstep(ex3))
   }
+
+  def specializeConc(s: Stmt): SAIDriver[Unit, Unit] = new StagedConcreteDriver {
+    @virtualize
+    def snippet(u: Rep[Unit]) = {
+      val res = runStmt(s)
+      println(res)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val s = Assign("x", Lit(1))
+    val code = specializeConc(s)
+    println(code.code)
+  }
+
 }
