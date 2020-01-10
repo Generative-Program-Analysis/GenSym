6fa1031afbf3b44249a13b3549012daeb820ab1f
Guannan Wei <guannanwei@outlook.com>
2020-01-10 17:31:13 -0500

    remove useless code

--- dev-clean/src/main/scala/sai/structure/domains/AbsDomain.scala
+++ dev-clean/src/main/scala/sai/structure/domains/AbsDomain.scala
@@ -5,20 +5,6 @@ import scala.language.higherKinds
 import scala.language.implicitConversions
 import scala.Double.{NegativeInfinity, PositiveInfinity}
 
-import lms.macros.SourceContext
-import lms.core.virtualize
-
-import sai.lmsx._
-
-trait NumAbsDomain {
-  type AD
-
-  def +(that: AD): AD
-  def -(that: AD): AD
-  def *(that: AD): AD
-  def /(that: AD): AD
-}
-
 trait NumD[T] {
   def +(x: T, y: T): T
   def -(x: T, y: T): T
