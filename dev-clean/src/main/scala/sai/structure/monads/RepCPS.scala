c01b6dbb68a1344fb5c2cda6c43539f059bf0828
Guannan Wei <guannanwei@outlook.com>
2020-01-14 11:16:34 -0500

    staged cps transformer

--- dev-clean/src/main/scala/sai/structure/monads/RepCPS.scala
+++ dev-clean/src/main/scala/sai/structure/monads/RepCPS.scala
@@ -32,4 +32,30 @@ trait RepCPSMonad { self: RepMonads with SAIOps =>
     def map[B: Manifest](f: Rep[A] => Rep[B]): CpsM[R, B] =
       CpsM[R, B]((k: Rep[B] => Rep[R]) => run(f.andThen(k)))
   }
+
+  object CpsT {
+    def apply[M[_]: Monad, R: Manifest, A: Manifest](implicit m: CpsT[M, R, A]) = m
+
+    implicit def CpsTMonadInstance[M[_]: Monad, R: Manifest] = new Monad[CpsT[M, R, ?]] {
+      def flatMap[A: Manifest, B: Manifest](fa: CpsT[M, R, A])(f: Rep[A] => CpsT[M, R, B]) =
+        fa.flatMap(f)
+      def pure[A: Manifest](a: Rep[A]): CpsT[M, R, A] = CpsT((k: Rep[A] => M[R]) => k(a))
+    }
+
+    def liftM[G[_]: Monad, R: Manifest, A: Manifest](g: G[A]): CpsT[G, R, A] =
+      CpsT[G, R, A]((k: Rep[A] => G[R]) => Monad[G].flatMap(g)(k))
+
+    def callcc[M[_]: Monad, T: Manifest, R: Manifest, A: Manifest](f: (Rep[T] => CpsT[M, R, A]) => CpsT[M, R, T]): CpsT[M, R, T] =
+      CpsT[M, R, T]((k: Rep[T] => M[R]) => f((x: Rep[T]) => CpsT[M, R, A]((_: Rep[A] => M[R]) => k(x))).run(k))
+  }
+
+  case class CpsT[M[_]: Monad, R: Manifest, A: Manifest](run: (Rep[A] => M[R]) => M[R]) {
+    import CpsT._
+
+    def apply(k: Rep[A] => M[R]): M[R] = run(k)
+    def flatMap[B: Manifest](f: Rep[A] => CpsT[M, R, B]): CpsT[M, R, B] =
+      CpsT[M, R, B]((k: Rep[B] => M[R]) => run(a => f(a).run(k)))
+    def map[B: Manifest](f: Rep[A] => Rep[B]): CpsT[M, R, B] =
+      CpsT[M, R, B]((k: Rep[B] => M[R]) => run(a => k(f(a))))
+  }
 }
