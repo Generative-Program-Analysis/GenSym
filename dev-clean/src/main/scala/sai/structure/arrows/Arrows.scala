6e121afc4a49075bd153f879dfaf5d92b4e5366e
Guannan Wei <guannanwei@outlook.com>
2020-01-16 17:07:29 -0500

    more arrows

--- dev-clean/src/main/scala/sai/structure/arrows/Arrows.scala
+++ dev-clean/src/main/scala/sai/structure/arrows/Arrows.scala
@@ -14,7 +14,15 @@ case class Kleisli[M[_], A, B](run: A => M[B]) {
   def apply(a: A): M[B] = run(a)
 }
 
-object Arrows {
+object Arrow {
+  def apply[A[_, _]](implicit a: Arrow[A]): Arrow[A] = a
+
+  implicit class ArrowOps[A[_, _]: Arrow, B, C](f: A[B, C]) {
+    def >>>[D](g: A[C, D]): A[B, D] = Arrow[A].>>>(f, g)
+    def ***[D, E](g: A[D, E]): A[(B, D), (C, E)] = __***(f, g)
+    def &&&[D](g: A[B, D]): A[B, (C, D)] = __&&&(f, g)
+  }
+
   def MonadIsArrow[M[_]: Monad] = new Arrow[Kleisli[M, ?, ?]] {
     def arr[B, C](f: B => C): Kleisli[M, B, C] = 
       Kleisli[M, B, C](b => Monad[M].pure(f(b)))
@@ -24,5 +32,29 @@ object Arrows {
       Kleisli[M, (B, D), (C, D)] { case (b, d) =>
         Monad[M].flatMap(f(b))(c => Monad[M].pure(c, d))
       }
+  }
+
+  def swap[A, B](ab: (A, B)): (B, A) = ab match { case (a, b) => (b, a) }
+
+  def snd[A[_, _]: Arrow, B, C, D](f: A[B, C]): A[(D, B), (D, C)] = {
+    val A = Arrow[A]; import A._
+    arr(swap[D, B]) >>> fst[B, C, D](f) >>> arr(swap[C, D])
+  }
+
+  def __***[A[_, _]: Arrow, B, C, D, E](f: A[B, C], g: A[D, E]): A[(B, D), (C, E)] = {
+    val A = Arrow[A]; import A._
+    fst(f) >>> snd(g)
+  }
+
+  def __&&&[A[_, _]: Arrow, B, C, D](f: A[B, C], g: A[B, D]): A[B, (C, D)] = {
+    val A = Arrow[A]; import A._
+    arr((b: B) => (b, b)) >>> (f *** g)
+  }
+
+  // An example
+
+  def add[A[_, _]: Arrow, B](f: A[B, Int], g: A[B, Int]): A[B, Int] = {
+    val A = Arrow[A]; import A._
+    (f &&& g) >>> arr { case (u: Int, v: Int) => u + v }
   }
 }
