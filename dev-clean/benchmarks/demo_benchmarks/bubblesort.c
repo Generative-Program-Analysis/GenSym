7d436f71f2b48d77eeae81e0209726dacacc7407
shangyint <tan279@purdue.edu>
2021-05-07 00:00:40 -0400

    bubbleSort

---
+++ dev-clean/benchmarks/demo_benchmarks/bubblesort.c
@@ -0,0 +1,28 @@
+#define SIZE 4
+
+void bubble_sort(int *d, int n)
+{
+	for (int k = 1; k < n; k++)
+	{
+		for (int i = 1; i < n; i++)
+		{
+			if (d[i] < d[i - 1])
+			{
+				int temp = d[i];
+				d[i] = d[i - 1];
+				d[i - 1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int data[SIZE];
+
+  make_symbolic(data, sizeof(int) * SIZE);
+	
+	bubble_sort(data, SIZE);
+	
+	return 0;
+}
