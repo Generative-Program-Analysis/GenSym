fcbb37d3949bb70770f545ce3bd832076ddfea1d
Guannan Wei <guannanwei@outlook.com>
2022-01-07 00:15:40 -0800

    missing file

---
+++ dev-clean/headers/llsc/misc.hpp
@@ -0,0 +1,21 @@
+#ifndef LLSC_END_HEADERS
+#define LLSC_END_HEADERS
+
+inline void prelude(int argc, char** argv) {
+  init_rand();
+  handle_cli_args(argc, argv);
+}
+
+inline void epilogue() {
+#ifdef USE_TP
+  tp.wait_for_tasks();
+#endif
+  cov.stop_monitor();
+  cov.print_time();
+  cov.print_block_cov();
+  cov.print_path_cov();
+  cov.print_async();
+  cov.print_query_stat();
+}
+
+#endif
