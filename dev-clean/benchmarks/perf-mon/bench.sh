c912442fc9f677310ab32a3a5dce3ab674a9ee79
Songlin <jia137@purdue.edu>
2022-02-26 23:37:59 -0500

    add notebook script

--- dev-clean/benchmarks/perf-mon/bench.sh
+++ dev-clean/benchmarks/perf-mon/bench.sh
@@ -2,7 +2,13 @@
 cd "$(dirname $0)"
 git pull --quiet --prune --recurse-submodules
 make
+
 cd ../..
 sbt "Bench / test"
 cat bench.csv >>~/.www/benchllsc/bench.csv
 rm bench.csv
+
+cd ~/.www/benchllsc
+conda run -n base \
+    jupyter nbconvert --to html --execute dataprocess.ipynb \
+                                --output  dataprocess.html
