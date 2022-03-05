a32627c996cb452669b3d6bee4c8fc0ed47241b2
Songlin <jia137@purdue.edu>
2022-03-04 23:18:41 -0500

    bench: fix branch checking

--- dev-clean/benchmarks/perf-mon/app.py
+++ dev-clean/benchmarks/perf-mon/app.py
@@ -18,7 +18,7 @@ def main_handler():
     client = Client.from_payload(payload)
     H = Handler()
 
-    @H.register(check_suite__app__slug="sai-bench", action="*requested")
+    @H.register(check_suite__app__slug="sai-bench", check_suite__head_branch="master", action="*requested")
     @H.register(check_run__app__slug="sai-bench", action="rerequested")
     def create_new_run():
         data = payload.get("check_run") or payload.get("check_suite")
