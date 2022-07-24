b873d2518713e961d448aef91b95817c24d0cb28
Guannan Wei <guannanwei@outlook.com>
2022-07-24 12:12:19 -0700

    consolidate makefile

--- dev-clean/grammar/generate.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-# Download ANTLR from https://www.antlr.org/download.html
-
-ANTLR4=antlr-4.9.3-complete.jar
-
-java -jar $ANTLR4 LLVMLexer.g4
-java -jar $ANTLR4 -visitor LLVMParser.g4
