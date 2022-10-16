a536deb2d89ed583129f8e6cf214a740180fba44
David Deng <daviddenghaotian@163.com>
2022-10-16 15:48:48 -0400

    fix test case

--- benchmarks/external-lib/stat_sym.c
+++ benchmarks/external-lib/stat_sym.c
@@ -21,10 +21,10 @@ int main()
     return 0;
   }
   // filename must point to a valid file from here
-  llsc_assert_eager(chmod(filename, 01721) == 0, "chmod should succeed");
-  llsc_assert_eager(chown(filename, 51, 93) == 0, "chown should be successful");
-  llsc_assert_eager(stat(filename, &sfile) == 0, "stat should be successful");
-  llsc_assert_eager((sfile.st_mode & 07777) == 01721, "chmod should take effect");
-  llsc_assert_eager(sfile.st_uid == 51 && sfile.st_gid == 93, "chmod should take effect");
+  gs_assert_eager(chmod(filename, 01721) == 0, "chmod should succeed");
+  gs_assert_eager(chown(filename, 51, 93) == 0, "chown should be successful");
+  gs_assert_eager(stat(filename, &sfile) == 0, "stat should be successful");
+  gs_assert_eager((sfile.st_mode & 07777) == 01721, "chmod should take effect");
+  gs_assert_eager(sfile.st_uid == 51 && sfile.st_gid == 93, "chmod should take effect");
   return 0;
 }
