73247995f97de1a69b3b819988785f30ee3b41f7
Mikail Khan <mikail@mikail-khan.com>
2023-07-14 02:09:09 -0400

    staged more complex wasm control flow

--- src/main/scala/wasm/SmallStagedEvalException.scala
+++ src/main/scala/wasm/SmallStagedEvalException.scala
@@ -26,17 +26,7 @@ trait StagedEval extends SAIOps {
 
   case class Frame(module: ModuleInstance, locals: Rep[List[Value]])
   case class Breaking(n: Int, stack: Rep[List[Value]]) extends Exception
-  // case class Continue(stack: Rep[List[Value]]) extends EvalResult
-  // case class Breaking(n: Int, stack: Rep[List[Value]]) extends EvalResult
-  // case class Returning(stack: Rep[List[Value]]) extends EvalResult
-  // abstract class EvalResult {
-  //   def onContinue(f: Rep[List[Value]] => EvalResult): EvalResult = {
-  //     this match {
-  //       case Continue(stack) => f(stack)
-  //       case _ => this
-  //     }
-  //   }
-  // }
+  case class Returning(stack: Rep[List[Value]]) extends Exception
 
   case class Config(frame: Frame, stackBudget: Int) {
     def evalBinOp(op: BinOp, lhsV: Rep[Value], rhsV: Rep[Value]): Rep[Value] = op match {
@@ -59,9 +49,6 @@ trait StagedEval extends SAIOps {
           val newStack = stack.drop(2)
           this.eval(evalBinOp(op, v1, v2) :: newStack, instrs.tail)
         }
-        case Br(label) => {
-          throw Breaking(label, stack)
-        }
 
         case If(blockTy, thenInstrs, elseInstrs) => {
           val cond: Rep[Int] = stack.head
@@ -69,15 +56,55 @@ trait StagedEval extends SAIOps {
           val funcType = blockTy.toFuncType(frame.module)
 
           val zero: Rep[Int] = 0
-          val condBool = cond == zero
-          val res = if (condBool) {
-            val retStack = evalBlock(funcType.out.length, elseInstrs)
-            this.eval(retStack ++ newStack, instrs.tail)
+          val retStack = if (cond == zero) {
+            evalBlock(funcType.out.length, elseInstrs)
           } else {
-            val retStack = evalBlock(funcType.out.length, thenInstrs)
-            this.eval(retStack ++ newStack, instrs.tail)
+            evalBlock(funcType.out.length, thenInstrs)
           }
-          res
+
+          this.eval(retStack ++ newStack, instrs.tail)
+        }
+        case Block(blockTy, blockInstrs) => {
+          val funcType = blockTy.toFuncType(frame.module)
+          val retStack = evalBlock(funcType.out.length, blockInstrs.toList)
+          this.eval(retStack ++ stack, instrs.tail)
+        }
+        case Loop(blockTy, loopInstrs) => {
+          val funcType = blockTy.toFuncType(frame.module)
+          val loopInstrList = loopInstrs.toList
+          def loop: Rep[List[Value] => List[Value]] = fun { loopStack =>
+            val blockStack = evalBlock(funcType.out.length, loopInstrList)
+            loop(loopStack ++ blockStack)
+          }
+          this.eval(loop(stack), instrs.tail)
+        }
+        case Br(label) => {
+          throw Breaking(label, stack)
+        }
+        case BrIf(label) => {
+          val cond: Rep[Int] = stack.head
+          val newStack = stack.tail
+          val zero: Rep[Int] = 0
+          if (cond == zero) {
+            this.eval(newStack, instrs.tail)
+          } else {
+            throw Breaking(label, newStack)
+          }
+        }
+        case Return => {
+          throw Returning(stack)
+        }
+        case Call(func) => {
+           val FuncDef(_, funcType, locals, body) = frame.module.funcs(func)
+           // TODO: reverse args
+           val args = stack.take(funcType.inps.length)
+           val newStack = stack.drop(funcType.inps.length)
+     
+           val initFrameLocals: Rep[List[Value]] = List.fill(locals.length)(I32(0))
+           val frameLocals: Rep[List[Value]] = args ++ initFrameLocals
+           val newFrame = Frame(frame.module, frameLocals)
+           val retStack = evalFunc(args, newFrame, funcType, body.toList)
+           this.eval(retStack ++ newStack, instrs.tail)
         }
       }
     }
@@ -89,6 +116,15 @@ trait StagedEval extends SAIOps {
       } catch {
         case Breaking(0, stack) => stack.take(outs)
         case Breaking(n, stack) => throw Breaking(n - 1, stack)
+        case e: Exception => throw e
+      }
+    }
+
+    def evalFunc(args: Rep[List[Value]], nframe: Frame, funcType: FuncType, instrs: List[Instr]): Rep[List[Value]] = {
+      try {
+        this.copy(frame = nframe).evalBlock(funcType.out.length, instrs)
+      } catch {
+        case Returning(retStack) => retStack.take(funcType.out.length)
         case e: Exception => throw e
       }
     }
@@ -112,13 +148,20 @@ object SmallStagedTest extends App {
 
   val module = {
     val types = List()
-    val funcs = List()
+    val funcs = List(
+      FuncDef("add5", FuncType(Seq(NumType(I32Type)), Seq(NumType(I32Type))), Seq(NumType(I32Type)), Seq(
+        Konst(I32C(5)), Binary(BinOp.Int(Add))
+      ))
+    )
     ModuleInstance(types, funcs)
   }
   val instrs = List(
-    // Konst(I32C(-5)),
+    Konst(I32C(10)),
+    Loop(ValBlockType(None), Seq(
+      Konst(I32C(1)), Binary(BinOp.Int(Add))
+    ))
     // Binary(BinOp.Int(Add)),
-    If(ValBlockType(Some(NumType(I32Type))), List(Konst(I32C(2999))), List(Br(0)))
+    // If(ValBlockType(Some(NumType(I32Type))), List(Konst(I32C(2999))), List(Br(0)))
   )
   val snip = mkVMSnippet(instrs, module)
   val code = snip.code
