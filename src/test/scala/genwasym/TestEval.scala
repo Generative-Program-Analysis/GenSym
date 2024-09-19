90fd9196b6a4c58b92a146b742430dea84e470d2
Guannan Wei <wei220@purdue.edu>
2024-09-19 13:42:40 +0200

    refactor test cases

--- src/test/scala/genwasym/TestEval.scala
+++ src/test/scala/genwasym/TestEval.scala
@@ -8,27 +8,21 @@ import gensym.wasm.source._
 import gensym.wasm.parser._
 import gensym.wasm.memory._
 import gensym.wasm.symbolic._
+import gensym.wasm.miniwasm._
+import collection.mutable.ArrayBuffer
 
 import org.scalatest.FunSuite
 
 class TestEval extends FunSuite {
 
   // Mostly testing the files generated form `benchmarks/wasm/test.rs`
-  def testfile(filename: String) = {
-    import gensym.wasm.miniwasm._
-    import collection.mutable.ArrayBuffer
+  def testFile(filename: String, main: String, expected: Option[Int] = None) = {
     val module = Parser.parseFile(filename)
 
-    val instrs = module.definitions
-      .find({
-        case FuncDef(Some("$real_main"), FuncBodyDef(_, _, _, _)) => true
-        case _                                                    => false
-      })
-      .map({
-        case FuncDef(_, FuncBodyDef(_, _, _, body)) => body
-      })
-      .get
-      .toList
+    val instrs = module.definitions.flatMap({
+        case FuncDef(Some(`main`), FuncBodyDef(_, _, _, body)) => body
+        case _ => List()
+    })
 
     val types = List()
     val funcs = module.definitions
@@ -48,31 +42,26 @@ class TestEval extends FunSuite {
       .toList
 
     val moduleInst = ModuleInstance(types, funcs, List(RTMemory()), globals)
+    val retK: Evaluator.RetCont = newStack => println(s"retCont: $newStack")
+    val trailK: Evaluator.Cont = newStack => {
+      println(s"trail: $newStack")
+      expected match {
+        case Some(e) => assert(newStack(0) == I32V(e))
+        case None    => ()
+      }
+    }
 
-    Evaluator.eval(instrs,
-                   List(),
-                   Frame(moduleInst, ArrayBuffer(I32V(0))),
-                   newStack => println(s"retCont: $newStack"),
-                   List(newStack => println(s"trail: $newStack")))
-
+    Evaluator.eval(instrs, List(), Frame(moduleInst, ArrayBuffer(I32V(0))), retK, List(trailK))
   }
 
-  // Mostly testing the files generated form `benchmarks/wasm/test.rs`
-  def test_btree(filename: String) = {
-    import gensym.wasm.miniwasm._
-    import collection.mutable.ArrayBuffer
+  // XXX (GW): remove this and just using testFile?
+  def test_btree(filename: String, main: String) = {
     val module = Parser.parseFile(filename)
 
-    val instrs = module.definitions
-      .find({
-        case FuncDef(Some("$main"), FuncBodyDef(_, _, _, _)) => true
-        case _                                               => false
-      })
-      .map({
-        case FuncDef(_, FuncBodyDef(_, _, _, body)) => body
-      })
-      .get
-      .toList
+    val instrs = module.definitions.flatMap({
+        case FuncDef(Some(`main`), FuncBodyDef(_, _, _, body)) => body
+        case _ => List()
+    })
 
     val types = List()
     val funcs = module.definitions
@@ -107,11 +96,15 @@ class TestEval extends FunSuite {
 
   }
 
-  test("ackermann") { testfile("./benchmarks/wasm/test_ack.wat") }
+  test("ackermann") {
+    testFile("./benchmarks/wasm/test_ack.wat", "$real_main", Some(7))
+  }
   // TODO: the power test can be used to test the stack
   // For now: 2^10 works, 2^100 results in 0 (why?),
   // and 2^1000 results in a stack overflow
-  test("power") { testfile("./benchmarks/wasm/test_pow.wat") }
+  test("power") {
+    testFile("./benchmarks/wasm/test_pow.wat", "$real_main", Some(1024))
+  }
 
   // TODO: fix this
   // test("btree") { test_btree("./benchmarks/wasm/btree/2o1u-no-label.wat") }
