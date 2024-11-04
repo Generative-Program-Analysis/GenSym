2eb36f046fe722ea38b080a905ded4800899789a
ahuoguo <52595524+ahuoguo@users.noreply.github.com>
2024-11-04 18:05:27 -0500

    Basic `(module bin ...)` support (#62)
    * basic .bin.wast working
    
    ---------
    
    Co-authored-by: butterunderflow <azhong.934@gmail.com>
    Co-authored-by: Guannan Wei <wei220@purdue.edu>

--- src/main/scala/wasm/Parser.scala
+++ src/main/scala/wasm/Parser.scala
@@ -3,6 +3,7 @@ package gensym.wasm.parser
 import gensym.wasm.ast._
 import gensym.wasm.source._
 
+import scala.util.Try
 import scala.util.parsing.combinator._
 import scala.util.parsing.input.Positional
 import scala.util.matching.Regex
@@ -13,6 +14,9 @@ import org.antlr.v4.runtime._
 import scala.collection.JavaConverters._
 import collection.mutable.{HashMap, ListBuffer}
 import gensym.wasm._
+
+import java.io.OutputStream
+
 
 import scala.collection.mutable
 
@@ -188,23 +192,78 @@ class GSWasmVisitor extends WatParserBaseVisitor[WIR] {
       ???
   }
 
+  // TODO: This doesn't seems quite correct
+  def parseHexFloat(text: String): Float = {
+    if (text.startsWith("0x") || text.startsWith("-0x") || text.startsWith("+0x")) {
+      // Remove optional sign and "0x" prefix
+      val cleanText = text.replaceFirst("^[+-]?0x", "")
+      // why removing the seemling irrelevant following two lines will effect
+      // the value being parsed?
+      val value: Float =  BigDecimal(text).floatValue
+      print(f"cleanText = $cleanText, value = $value\n")
+
+      val Array(mantissa, exponent) = cleanText.split("p", 2)
+
+      // Convert mantissa and exponent
+      val mantissaValue = java.lang.Float.intBitsToFloat(java.lang.Integer.parseUnsignedInt(mantissa.replace(".", ""), 16))
+      val exponentValue = Math.pow(2, exponent.toInt).toFloat
+      // print(s"mantissaValue = $mantissaValue, exponentValue = $exponentValue\n")
+      mantissaValue * exponentValue
+    } else {
+      text.toFloat // Fall back to regular decimal parsing
+    }
+  }
+
+
   def visitLiteralWithType(ctx: LiteralContext, ty: NumType): Num = {
     if (ctx.NAT != null) {
       ty.kind match {
-        case I32Type => I32V(ctx.NAT.getText.toInt)
-        case I64Type => I64V(ctx.NAT.getText.toLong)
+        case I32Type => {
+          if (ctx.NAT.getText.startsWith("0x")) {
+            I32V(Integer.parseInt(ctx.NAT.getText.substring(2), 16))
+          } else {
+            I32V(ctx.NAT.getText.toInt)
+          }
+        }
+        case I64Type => {
+          if (ctx.NAT.getText.startsWith("0x")) {
+            I64V(java.lang.Long.parseLong(ctx.NAT.getText.substring(2), 16))
+          } else {
+            I64V(ctx.NAT.getText.toLong)
+          }
+        }
       }
     } else if (ctx.INT != null) {
       ty.kind match {
-        case I32Type => I32V(ctx.INT.getText.toInt)
-        case I64Type => I64V(ctx.INT.getText.toLong)
+        case I32Type => {
+          if (ctx.INT.getText.startsWith("0x")) {
+            I32V(Integer.parseInt(ctx.INT.getText.substring(2), 16))
+          } else {
+            I32V(ctx.INT.getText.toInt)
+          }
+        }
+        case I64Type => {
+          if (ctx.INT.getText.startsWith("0x")) {
+            I64V(java.lang.Long.parseLong(ctx.INT.getText.substring(2), 16))
+          } else {
+            I64V(ctx.INT.getText.toLong)
+          }
+        }
       }
+    // TODO: parsing support for hex representation for f32/f64 not quite there yet
     } else if (ctx.FLOAT != null) {
       ty.kind match {
-        case F32Type => F32V(ctx.FLOAT.getText.toFloat)
-        case F64Type => F64V(ctx.FLOAT.getText.toDouble)
+        case F32Type => 
+          val parsedValue = Try(parseHexFloat(ctx.FLOAT.getText).toFloat).getOrElse(ctx.FLOAT.getText.toFloat)
+          F32V(parsedValue)
+          
+        case F64Type => 
+          // TODO: not processed at all
+          val parsedValue = ctx.FLOAT.getText.toDouble
+          F64V(parsedValue)
       }
-    } else error
+    }
+    else error
   }
 
   override def visitPlainInstr(ctx: PlainInstrContext): Instr = {
@@ -635,10 +694,45 @@ class GSWasmVisitor extends WatParserBaseVisitor[WIR] {
     else error
   }
 
-  override def visitScriptModule(ctx: ScriptModuleContext): Module = {
+ override def visitScriptModule(ctx: ScriptModuleContext): Module = {
     if (ctx.module_ != null) {
       visitModule_(ctx.module_).asInstanceOf[Module]
-    } else {
+    }
+    else if (ctx.BIN != null) {
+
+      val bin = ctx.STRING_
+      val hexString = bin.asScala.toList.map(_.getText.substring(1).dropRight(1)).mkString
+
+      val byteArray: Array[Byte] = hexStringToByteArray(hexString)
+
+      // just for fact checking
+      // val filePath = "temp.bin"
+      // Files.write(Paths.get(filePath), byteArray)
+
+      // use `wasmfx-tools` to convert the binary file to a text file
+      val processBuilder = new ProcessBuilder("./third-party/wasmfx-tools/target/release/wasm-tools", "print")
+
+      val process = processBuilder.start()
+      val outputStream: OutputStream = process.getOutputStream
+      try {
+        outputStream.write(byteArray)
+        outputStream.flush()
+      } finally {
+        outputStream.close() // Close the stream to signal end of input
+      }
+
+      val output = scala.io.Source.fromInputStream(process.getInputStream).mkString
+      val errorOutput = scala.io.Source.fromInputStream(process.getErrorStream).mkString
+      val exitCode = process.waitFor()
+
+      // println(s"Exit code: $exitCode")
+      // println(s"Output:\n$output")
+      // println(s"Error Output:\n$errorOutput")
+
+      val module = Parser.parse(output)
+      module
+    }
+    else {
       throw new RuntimeException("Unsupported")
     }
   }
@@ -688,13 +782,19 @@ class GSWasmVisitor extends WatParserBaseVisitor[WIR] {
     Script(cmds.toList)
   }
 
-  override def visitTag(ctx: TagContext): WIR = {
-    val name = getVar(ctx.bindVar)
-    val ty = visitFuncType(ctx.funcType)
-    Tag(name, ty)
+  // Function to convert a hex string representation to an Array[Byte]
+  def hexStringToByteArray(hex: String): Array[Byte] = {
+    // Split the input string by '\' and filter out empty strings
+    val byteStrings = hex.split("\\\\").filter(_.nonEmpty)
+
+    byteStrings.map { byteStr =>
+      // Parse the hex value to a byte
+      Integer.parseInt(byteStr, 16).toByte
+    }
   }
 
 }
+
 
 object Parser {
   private def makeWatVisitor(input: String) = {
