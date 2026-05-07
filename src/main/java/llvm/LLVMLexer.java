d3483f3784867987a7e3393990d6749e7527fb25
Guannan Wei <guannan.wei@tufts.edu>
2026-05-07 00:13:03 -0400

    make sure using the same ANTLR

--- src/main/java/llvm/LLVMLexer.java
+++ src/main/java/llvm/LLVMLexer.java
@@ -1,5 +1,5 @@
+// Generated from LLVMLexer.g4 by ANTLR 4.13.0
 package gensym.llvm;
-// Generated from LLVMLexer.g4 by ANTLR 4.12.0
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
@@ -11,7 +11,7 @@ import org.antlr.v4.runtime.misc.*;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue"})
 public class LLVMLexer extends Lexer {
-	static { RuntimeMetaData.checkVersion("4.12.0", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.13.0", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
--- src/main/java/wasm/WatLexer.java
+++ src/main/java/wasm/WatLexer.java
@@ -1,5 +1,5 @@
-package gensym.wasm;
 // Generated from WatLexer.g4 by ANTLR 4.13.0
+package gensym.wasm;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
