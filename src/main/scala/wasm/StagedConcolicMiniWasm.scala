ebf3c47e691c2945c809b0779d91ce993c435718
butterunderflow <azhong.934@gmail.com>
2026-06-06 19:03:36 -0400

    remove unused include folders

--- src/main/scala/wasm/StagedConcolicMiniWasm.scala
+++ src/main/scala/wasm/StagedConcolicMiniWasm.scala
@@ -2249,7 +2249,7 @@ trait StagedWasmCppGen extends CGenBase with CppSAICodeGenBase {
   includePaths.clear()
   headers.clear()
 
-  registerHeader("headers", "\"wasm.hpp\"")
+  registerHeader("\"wasm.hpp\"")
   registerHeader("<functional>")
   registerHeader("<stdbool.h>")
   registerHeader("<stdint.h>")
