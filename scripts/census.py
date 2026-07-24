6dd0dcaa67975d71b7007213cd2c7122db699ac9
butterunderflow <azhong.934@gmail.com>
2026-07-24 01:03:36 -0400

    delete more from git

--- scripts/census.py
+++
@@ -1,7 +0,0 @@
-ll_filename = "echo.ll"
-
-with open(ll_filename) as f:
-    read_data = f.read()
-    declared_fun = [ l.split("@")[1].split("(")[0] for l in read_data.splitlines() if "declare" in l ]
-    for fun_name in declared_fun:
-        print(fun_name)
